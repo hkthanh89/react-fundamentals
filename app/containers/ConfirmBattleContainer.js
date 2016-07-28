2c844d9898d5e372708d5369dba7511d6a96865f
Thanh Huynh Kien <hkthanh89@gmail.com>
2016-07-28 11:44:39 +0700

    Axios, Promises and Github API

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -17,8 +18,18 @@ var ConfirmBattleContainer = React.createClass({
   },
   componentDidMount: function () {
     var query = this.props.location.query;
-    console.log('componentDidMount')
+    console.log('componentDidMount');
+    console.log('QUERY', query);
     // Fetch info from github then update state
+    // https://egghead.io/playlists/the-this-key-word-250c37d9
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function (players) {
+        console.log('PLAYERS', players)
+        this.setState({
+          isLoading: false,
+          playerInfo: [players[0], players[1]]
+        })
+      }.bind(this))
   },
   componentWillReceiveProps: function () {
     console.log('componentWillReceiveProps')
