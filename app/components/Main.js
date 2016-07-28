05f48e50d465e7117940849968b934f7e3ca928e
Thanh Huynh Kien <hkthanh89@gmail.com>
2016-07-29 02:06:20 +0700

    React cloneElement

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,13 +1,20 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css');
 
 var Main = React.createClass({
   render: function() {
     return (
       <div className='main-container'>
-        {this.props.children}
-      </div>
-    )
-  }
+        <ReactCSSTransitionGroup
+          transitionName="appear"
+          transitionEnterTimeout={800}
+          transitionLeaveTimeout={800}>
+          {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+      </ReactCSSTransitionGroup>
+    </div>
+  )
+}
 });
 
 module.exports = Main;
